f72ccf5060896cbf8e1a28dcea483418c00f4e04
Trongtangoc <lenhisomot@gmail.com>
2025-05-26 15:26:51 +0700

    convertCurrency

---
+++ CurrencyConverterController/src/main/java/com/codegym/currencyconvertercontroller/CurrencyConverterController.java
@@ -0,0 +1,25 @@
+package com.codegym.currencyconvertercontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CurrencyConverterController {
+    @GetMapping("/")
+    public String showForm(){
+        return "convert-form";
+    }
+    @PostMapping ("/convert")
+    public String convertCurrency(@RequestParam("rate") double rate,
+                                  @RequestParam("usd") double usd,
+        Model model) {
+        double vnd = rate * usd;
+        model.addAttribute("rate", rate);
+        model.addAttribute("usd", usd);
+        model.addAttribute("vnd", vnd);
+        return "convert-result";
+    }
+}
